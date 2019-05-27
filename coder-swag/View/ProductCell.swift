065711fdd700c9802717642cfb42d31ca493e796
osamah morished <omorished@gmail.com>
2019-05-27 09:55:26 +0300

    create ProductCell class and connect the IBOutlet to it

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Os! on 27/05/2019.
+//  Copyright © 2019 Os!. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    //IBOutlet Connections
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
